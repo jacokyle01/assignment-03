f3501eeeaf71e3e29aba7d16af050fe753832eda
Kyle Jacobson <jacokyle01@gmail.com>
2024-04-22 13:32:41 -0500

    format + lint fixes + try to fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,57 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [view, setView] = useState("readProducts");
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [dataF, setDataF] = useState({});
-  
-  const renderProducts = () => {
-    let response = fetch("http://localhost:8081/products");
-    let products = response.json();
+	// addProduct, updatePrice, deleteProduct, studentInfo
+	const [view, setView] = useState("readProducts");
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const [dataF, setDataF] = useState({});
 
-    return (
-      <div id="product-wrap">
-        {products.map((product) => (
-          <div id="product">
-            <img src={product.image}/>
-            <h1>${product.title}</h1>
-            <h2>${product.price}</h2>
-            <p>${product.description}</p>
-          </div>
-        ))};
-      </div>
-    )
-  };
 
-  const readProductsView = () => {
-    return (
+  const fetchProducts = async () => {
+    return fetch("http://localhost:3001/products").then(data => data.json);
+  }
+
+	const renderProducts = () => {
+    const products = fetchProducts();
+    console.log(products);
+		return (
+			<div id="product-wrap">
+				{products.map((product) => (
+					<div id="product">
+						<img src={product.image} />
+						<h1>${product.title}</h1>
+						<h2>${product.price}</h2>
+						<p>${product.description}</p>
+					</div>
+				))}
+				;
+			</div>
+		);
+	};
+
+	const readProductsView = () => {
+		return (
 			<div id="main">
-				<header className='header'>
-					<button 
-						className="button" 
-						onClick={() => setView("addProduct")}>
+				<header className="header">
+					<button className="button" onClick={() => setView("addProduct")}>
 						{" "}
 						Create Product
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("updatePrice")}>
+					<button className="button" onClick={() => setView("updatePrice")}>
 						{" "}
 						Update Price
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("deleteProduct")}>
+					<button className="button" onClick={() => setView("deleteProduct")}>
 						{" "}
 						Delete Product
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("studentInfo")}>
+					<button className="button" onClick={() => setView("studentInfo")}>
 						{" "}
 						Meet the Authors
 					</button>
@@ -60,80 +59,92 @@ function App() {
 				<main>{renderProducts()}</main>
 			</div>
 		);
-  };
+	};
 
-  const renderAddProduct = () => {
-    const onSubmit = (data) => {
-		  console.log({ data });
-		  setDataF(data);
+	const renderAddProduct = () => {
+		const onSubmit = (data) => {
+			console.log({ data });
+			setDataF(data);
 		};
 
-      return (
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
-            <div className="form-group">
-              <input {...register("title", { required: true })} placeholder="Product Title" />
-              {errors.title && <p>Product title is required.</p>}
-            </div>
-            <div className="form-group">
-              <input {...register("price", { required: true, pattern: /^\d+\.\d{2}$/ })} placeholder="Product Price" />
-              {errors.price && <p>Valid product price is required.</p>}
-            </div>
-            <div className="form-group">
-              <input {...register("description", { required: true })} placeholder="Product Description" />
-              {errors.description && <p>Product description is required.</p>}
-            </div>
-            <div className="form-group">
-              <select {...register("category", { required: true })} placeholder="Product Rating">
-                <option value="">Select Product Rating</option>
-                <option value="menCloth">Men's Clothing</option>
-                <option value="womenCloth">Women's Clothing</option>
-                <option value="jewelry">Jewelry</option>
-                <option value="elec">Electronics</option>
-              </select>
-              {errors.rating && <p>Product rating is required.</p>}
-            </div>
-            <div className="form-group">
-              <select {...register("image", { required: true })} placeholder="Product Image">
-                <option value="">Select Product Image</option>
-                <option value="menShirt">Men's Slim Fit T-Shirt</option>
-                <option value="menJacket">Men's Jacket</option>
-                <option value="womenShirt">Women's Shirt</option>
-                <option value="womenJacket">Women's Raincoat</option>
-              </select>
-              {errors.rating && <p>Product rating is required.</p>}
-            </div>
-            <button type="submit" className="button-add-product">Create Product</button>
-          </form>
-        </div>
-      )
-  };
+		return (
+			<div>
+				<form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+					<div className="form-group">
+						<input
+							{...register("title", { required: true })}
+							placeholder="Product Title"
+						/>
+						{errors.title && <p>Product title is required.</p>}
+					</div>
+					<div className="form-group">
+						<input
+							{...register("price", {
+								required: true,
+								pattern: /^\d+\.\d{2}$/,
+							})}
+							placeholder="Product Price"
+						/>
+						{errors.price && <p>Valid product price is required.</p>}
+					</div>
+					<div className="form-group">
+						<input
+							{...register("description", { required: true })}
+							placeholder="Product Description"
+						/>
+						{errors.description && <p>Product description is required.</p>}
+					</div>
+					<div className="form-group">
+						<select
+							{...register("category", { required: true })}
+							placeholder="Product Rating"
+						>
+							<option value="">Select Product Rating</option>
+							<option value="menCloth">Men's Clothing</option>
+							<option value="womenCloth">Women's Clothing</option>
+							<option value="jewelry">Jewelry</option>
+							<option value="elec">Electronics</option>
+						</select>
+						{errors.rating && <p>Product rating is required.</p>}
+					</div>
+					<div className="form-group">
+						<select
+							{...register("image", { required: true })}
+							placeholder="Product Image"
+						>
+							<option value="">Select Product Image</option>
+							<option value="menShirt">Men's Slim Fit T-Shirt</option>
+							<option value="menJacket">Men's Jacket</option>
+							<option value="womenShirt">Women's Shirt</option>
+							<option value="womenJacket">Women's Raincoat</option>
+						</select>
+						{errors.rating && <p>Product rating is required.</p>}
+					</div>
+					<button type="submit" className="button-add-product">
+						Create Product
+					</button>
+				</form>
+			</div>
+		);
+	};
 
-  const addProductView = () => {
-    return (
+	const addProductView = () => {
+		return (
 			<div id="main">
-				<header className='header'>
-					<button 
-						className="button" 
-						onClick={() => setView("readProducts")}>
+				<header className="header">
+					<button className="button" onClick={() => setView("readProducts")}>
 						{" "}
 						See Products
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("updatePrice")}>
+					<button className="button" onClick={() => setView("updatePrice")}>
 						{" "}
 						Update Price
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("deleteProduct")}>
+					<button className="button" onClick={() => setView("deleteProduct")}>
 						{" "}
 						Delete Product
 					</button>
-          <button 
-						className="button" 
-						onClick={() => setView("studentInfo")}>
+					<button className="button" onClick={() => setView("studentInfo")}>
 						{" "}
 						Meet the Authors
 					</button>
@@ -141,51 +152,29 @@ function App() {
 				<main>{renderAddProduct()}</main>
 			</div>
 		);
-  };
+	};
 
-  const renderView = () => {
+	const renderView = () => {
 		switch (view) {
 			case "addProduct":
 				return addProductView();
 			case "readProducts":
 				return readProductsView();
 			case "updatePrice":
-				return updatePriceView();
-      case "deleteProduct":
-        return deleteProductView();
-      case "studentInfo":
-        return studentInfoView();
+				// return updatePriceView();
+				break;
+			case "deleteProduct":
+				// return deleteProductView();
+				break;
+			case "studentInfo":
+				// return studentInfoView();
+				break;
 			default:
 				return;
 		}
 	};
 
-  return <div id="main">{renderView()}</div>;
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
+	return <div id="main">{renderView()}</div>;
 }
 
-export default App
+export default App;
