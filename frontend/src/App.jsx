9e5f7a01ddc58cb1832283070f5a23aba3b3f9c3
Simon Berberich <simonb@iastate.edu>
2024-04-22 13:07:51 -0500

    Started on creating products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,7 +7,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [view, setView] = useState("readProducts");
-
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [dataF, setDataF] = useState({});
+  
   const renderProducts = () => {
     let response = fetch("http://localhost:8081/products");
     let products = response.json();
@@ -15,13 +18,14 @@ function App() {
       <div id="product-wrap">
         {products.map((product) => (
           <div id="product">
-            <h2>${product.title}</h2>
-
+            <img src={product.image}/>
+            <h1>${product.title}</h1>
+            <h2>${product.price}</h2>
+            <p>${product.description}</p>
           </div>
         ))};
       </div>
     )
-
   };
 
   const readProductsView = () => {
@@ -54,6 +58,87 @@ function App() {
 					</button>
 				</header>
 				<main>{renderProducts()}</main>
+			</div>
+		);
+  };
+
+  const renderAddProduct = () => {
+    const onSubmit = (data) => {
+		  console.log({ data });
+		  setDataF(data);
+		};
+
+      return (
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+            <div className="form-group">
+              <input {...register("title", { required: true })} placeholder="Product Title" />
+              {errors.title && <p>Product title is required.</p>}
+            </div>
+            <div className="form-group">
+              <input {...register("price", { required: true, pattern: /^\d+\.\d{2}$/ })} placeholder="Product Price" />
+              {errors.price && <p>Valid product price is required.</p>}
+            </div>
+            <div className="form-group">
+              <input {...register("description", { required: true })} placeholder="Product Description" />
+              {errors.description && <p>Product description is required.</p>}
+            </div>
+            <div className="form-group">
+              <select {...register("category", { required: true })} placeholder="Product Rating">
+                <option value="">Select Product Rating</option>
+                <option value="menCloth">Men's Clothing</option>
+                <option value="womenCloth">Women's Clothing</option>
+                <option value="jewelry">Jewelry</option>
+                <option value="elec">Electronics</option>
+              </select>
+              {errors.rating && <p>Product rating is required.</p>}
+            </div>
+            <div className="form-group">
+              <select {...register("image", { required: true })} placeholder="Product Image">
+                <option value="">Select Product Image</option>
+                <option value="menShirt">Men's Slim Fit T-Shirt</option>
+                <option value="menJacket">Men's Jacket</option>
+                <option value="womenShirt">Women's Shirt</option>
+                <option value="womenJacket">Women's Raincoat</option>
+              </select>
+              {errors.rating && <p>Product rating is required.</p>}
+            </div>
+            <button type="submit" className="button-add-product">Create Product</button>
+          </form>
+        </div>
+      )
+  };
+
+  const addProductView = () => {
+    return (
+			<div id="main">
+				<header className='header'>
+					<button 
+						className="button" 
+						onClick={() => setView("readProducts")}>
+						{" "}
+						See Products
+					</button>
+          <button 
+						className="button" 
+						onClick={() => setView("updatePrice")}>
+						{" "}
+						Update Price
+					</button>
+          <button 
+						className="button" 
+						onClick={() => setView("deleteProduct")}>
+						{" "}
+						Delete Product
+					</button>
+          <button 
+						className="button" 
+						onClick={() => setView("studentInfo")}>
+						{" "}
+						Meet the Authors
+					</button>
+				</header>
+				<main>{renderAddProduct()}</main>
 			</div>
 		);
   };
